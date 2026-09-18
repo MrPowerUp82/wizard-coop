d377274b8ccd606cf45791c0005085c3e1187662
MrPowerUp82 <gustavohenrique8282@hotmail.com>
2026-09-18 17:10:00 -0300

    fix(vita): compile valid FSELF eboot.bin via VitaSDK and track in runtime/

--- platforms/vita/runtime/src/main.c
+++ platforms/vita/runtime/src/main.c
@@ -14,7 +14,7 @@
 #include <psp2/power.h>
 #include <psp2/touch.h>
 #include <psp2/display.h>
-#include <psp2/kernel/process.h>
+#include <psp2/kernel/processmgr.h>
 #include <psp2/kernel/clib.h>
 #include <psp2/audioout.h>
 
